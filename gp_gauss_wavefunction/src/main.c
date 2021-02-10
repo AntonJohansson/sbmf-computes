fca6f9a9d7e1496079ebca079ed9afc0e3b2aa3e
Anton Johansson <97antjoh@gmail.com>
2021-02-10 15:33:12 +0100

    sync

--- gp_gauss_wavefunction/src/main.c
+++ gp_gauss_wavefunction/src/main.c
@@ -31,16 +31,17 @@ int main() {
 	};
 	struct nlse_guess* default_guesses = NULL;
 
-	f64 g0 = 4.0/3.0;
+	f64 lambda = -1.0;
 	i64 N  = 4;
+	f64 g0 = lambda/((f64)N-1.0);
 
 	struct nlse_settings settings = {
         .spatial_pot_perturbation = perturbation,
 		.max_iterations = 1e5,
-		.max_integration_evals = 1e5,
+		.max_quadgk_iters = 500,
 		.error_tol = 1e-14,
 
-        .num_basis_funcs = 16,
+        .num_basis_funcs = 48,
 		.basis = ho_basis,
 		.hamiltonian_mixing = 0.5,
 
@@ -54,43 +55,6 @@ int main() {
 	struct nlse_result gp_default_res = grosspitaevskii(settings, component_count, &N, default_guesses, &g0);
 	struct nlse_result gp_random_res = grosspitaevskii(settings, component_count, &N, random_guesses, &g0);
 
-	f64 Egp_default = grosspitaevskii_energy(settings, gp_default_res.coeff_count, component_count, gp_default_res.coeff, &N, &g0);
-	f64 Egp_random  = grosspitaevskii_energy(settings, gp_random_res.coeff_count, component_count, gp_random_res.coeff, &N, &g0);
-
-	struct bestmf_result bmf_default = best_meanfield(settings, N, g0, default_guesses);
-	struct bestmf_result bmf_random = best_meanfield(settings, N, g0, random_guesses);
-
-	struct pt_result rs_default_ptres = rayleigh_schroedinger_pt_rf(settings, gp_default_res, 0, &g0, &N);
-	struct pt_result en_default_ptres = en_pt_rf(settings, gp_default_res, 0, &g0, &N);
-
-	struct pt_result rs_random_ptres = rayleigh_schroedinger_pt_rf(settings, gp_random_res, 0, &g0, &N);
-	struct pt_result en_random_ptres = en_pt_rf(settings, gp_random_res, 0, &g0, &N);
-
-	{
-		FILE* fd = fopen("out_E", "a");
-		fprintf(fd, "# mu\tE\tBMF\tRS2\tRS3\tEN2\tEN3\n");
-		fprintf(fd, "#default\n%.10e\t%.10e\t%.10e\t%.10e\t%.10e\t%.10e\t%.10e\n",
-				gp_default_res.energy[0],
-				Egp_default,
-				bmf_default.energy,
-				rs_default_ptres.E0 + rs_default_ptres.E1 + rs_default_ptres.E2,
-				rs_default_ptres.E0 + rs_default_ptres.E1 + rs_default_ptres.E2 + rs_default_ptres.E3,
-				en_default_ptres.E0 + en_default_ptres.E1 + en_default_ptres.E2,
-				en_default_ptres.E0 + en_default_ptres.E1 + en_default_ptres.E2 + en_default_ptres.E3
-				);
-		fprintf(fd, "#random\n%.10e\t%.10e\t%.10e\t%.10e\t%.10e\t%.10e\t%.10e\n",
-				gp_random_res.energy[0],
-				Egp_random,
-				bmf_random.energy,
-				rs_random_ptres.E0 + rs_random_ptres.E1 + rs_random_ptres.E2,
-				rs_random_ptres.E0 + rs_random_ptres.E1 + rs_random_ptres.E2 + rs_random_ptres.E3,
-				en_random_ptres.E0 + en_random_ptres.E1 + en_random_ptres.E2,
-				en_random_ptres.E0 + en_random_ptres.E1 + en_random_ptres.E2 + en_random_ptres.E3
-				);
-
-		fclose(fd);
-	}
-
 	{
 		FILE* fd = fopen("out_W", "a");
 		fprintf(fd, "# x\tpotential\tdefault\trandom\n");
@@ -102,8 +66,8 @@ int main() {
 		  ho_sample(gp_random_res.coeff_count, &gp_random_res.coeff[0], 1, &random_out, &x);
 		  f64 pot = 0.5*x*x + PERTURBATION(x);
 		  fprintf(fd, "%lf\t%lf\t%lf\t%lf\n", x, pot,
-				  gp_default_res.energy[0] + default_out*default_out,
-				  gp_random_res.energy[0] + random_out*random_out);
+				  default_out*default_out,
+				  random_out*random_out);
 		}
 
 		fclose(fd);
