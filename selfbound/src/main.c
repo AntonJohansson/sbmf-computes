d3d7e705ae0ace888abcf10eb317edb79adbf026
Anton Johansson <97antjoh@gmail.com>
2021-03-29 00:58:29 +0200

    sync

--- selfbound/src/main.c
+++ selfbound/src/main.c
@@ -96,7 +96,7 @@ int main() {
 	//f64 Os[] = {0.3, 0.2, 0.15, 0.1};
 	//f64 Os[] = {0.1, 0.05, 0.025};
 	f64 Os[] = {0.0020};
-	f64 gAB_factors[] = {-0.95,-1.00,-1.05};
+	f64 gAB_factors[] = {-1.10,-1.15};
 
 	f64 lambda = 0.05;
 	for (u32 k = 0; k < sizeof(gAB_factors)/sizeof(gAB_factors[0]); ++k) {
